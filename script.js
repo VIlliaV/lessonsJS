ffce178324be3c0180ecc1c368f0b7a7f68a7aa1
Illia <illia.vasiliev@gmail.com>
2022-12-11 19:47:05 +0200

    start

---
+++ script.js
@@ -0,0 +1,9 @@
+// !============= модуль 1 ========
+// ? № 1
+
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+console.log(total);
+const diff = apples - grapes;
+console.log(diff);
