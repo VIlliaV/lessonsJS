ec77881b144008e124a6483dba7d7b54debf3963
Illia <illia.vasiliev@gmail.com>
2022-12-11 19:54:43 +0200

    first done

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // !============= модуль 1 ========
 // ? № 1
 
-const apples = 47;
-const grapes = 135;
-const total = apples + grapes;
-console.log(total);
-const diff = apples - grapes;
-console.log(diff);
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff);
+
+const go = 5;
