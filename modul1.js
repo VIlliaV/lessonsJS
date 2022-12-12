7507d7b982734791a19b30870088a302cd24cf89
Illia <illia.vasiliev@gmail.com>
2022-12-12 20:32:07 +0200

    start module2

--- modul1.js
+++ modul1.js
@@ -53,17 +53,48 @@
 //     тобто як роздільник дробової частини може бути кома.
 // Індекс маси тіла необхідно округлити до однієї цифри після коми;
 
-let weight = "88,3";
-let height = "1.75";
-function ifStringToNumber(string) {
-  if (Number(string)) {
-    return string;
-  } else {
-    return Number(string.replace(",", "."));
-  }
-}
-function bmi(weight, height) {
-  return (ifStringToNumber(weight) / ifStringToNumber(height) ** 2).toFixed(1);
-}
+// let weight = "88,3";
+// let height = "1.75";
+// function ifStringToNumber(string) {
+//   if (Number(string)) {
+//     return string;
+//   } else {
+//     return Number(string.replace(",", "."));
+//   }
+// }
+// function bmi(weight, height) {
+//   return (ifStringToNumber(weight) / ifStringToNumber(height) ** 2).toFixed(1);
+// }
 
-console.log(bmi(weight, height)); // 28.8
+// console.log(bmi(weight, height)); // 28.8
+
+// ? #9
+// Отрефактори код так, щоб у змінну value присвоювалося значення
+// змінної incomingValue, якщо воно не рівне undefined або null.
+// В іншому випадку має присвоюватися значення defaultValue.
+// Перевір роботу скрипта для наступних значень змінної incomingValue:
+// null, undefined, 0, false.
+// Використовуй оператор ?? (nullish coalescing operator).
+
+// const incomingValue = null;
+// const defaultValue = 10;
+// const value = incomingValue ?? defaultValue;
+// console.log(value);
+
+// ? #10
+// Напиши скрипт, який переведе значення totalMinutes(кількість хвилин)
+//  в рядок у форматі годин та хвилин HH: MM.
+
+// 70 покаже 01:10
+// 450 покаже 07:30
+// 1441 покаже 24:01
+// const totalMinutes = 181;
+
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
